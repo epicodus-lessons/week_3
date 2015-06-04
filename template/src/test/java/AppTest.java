fd68864b8be6855c7d13fe1cf82b52d6a0a2090d
Jake Kaad <kaadalac@gmail.com>
2015-06-04 15:32:10 -0700

    create a week 3 template

--- template/src/test/java/AppTest.java
+++ template/src/test/java/AppTest.java
@@ -1,6 +1,27 @@
-import org.junit.*;
-import static org.junit.Assert.*;
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
-public class AppTest {
-  Sql2o sql2o = new Sql2o("jdbc:mysql://localhost:5432/todo-development", "jake", "password");
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
+  public WebDriver getDefaultDriver() {
+      return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Rule
+  public DataBaseRule database = new DataBaseRule();
+
+  @Test
+  // firsttesthere!
 }
